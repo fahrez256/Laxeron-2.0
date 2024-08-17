0fba5d14b024a1e836bd6bc5e419d5c13daaf61c
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-08-17 19:39:06 +0700

    Update source.sh

--- fun/source.sh
+++ fun/source.sh
@@ -11,7 +11,7 @@ mkdir -p "$LAXMODULE"
 local functionApi="https://raw.githubusercontent.com/fahrez256/Laxeron-2.0/main/fun/function.sh"
 local responsePath="/sdcard/Android/data/${LAXPKG}/files/response"
 local errorPath="/sdcard/Android/data/${LAXPKG}/files/error"
-am startservice -n ${LAXPKG}/.Storm --es api "$functionApi" #> /dev/null 2>&1
+am startservice -n "${LAXPKG}/.Storm" --es api "$functionApi" #> /dev/null 2>&1
 echo $responsePath
 #while [ ! -e "$responsePath" ] && [ ! -e "$errorPath" ]; do
 #done
