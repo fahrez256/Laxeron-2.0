db498ee3a0a5fb9451a7481d3e1a977dff1f079a
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-08-17 21:30:40 +0700

    Update source.sh

--- fun/source.sh
+++ fun/source.sh
@@ -3,23 +3,23 @@ export LAXID="!axId"
 export LAXVNAME="!axVName"
 export LAXVCODE="!axVCode"
 export LAXPATH="/sdcard/Android/data/${LAXPKG}/files"
-export LAXMAIN="https://raw.githubusercontent.com/fahrez256/Laxeron-2.0/main"
-export LAXBIN="/data/local/tmp/lax_bin"
-export LAXCASH="/data/local/tmp/lax_cash"
-export LAXMODULE="/sdcard/AxModules"
-export LAXFUNPATH="${LAXBIN}/function"
-export LAXFUN="source $LAXFUNPATH"
-export LAXPROP="${LAXMODULE}/.prop"
+export LAXMAINPATH="https://raw.githubusercontent.com/fahrez256/Laxeron-2.0/main"
+export LAXBINPATH="/data/local/tmp/lax_bin"
+export LAXCASHPATH="/data/local/tmp/lax_cash"
+export LAXMODULEPATH="/sdcard/AxModules"
+export LAXFUNLOC="${LAXBINPATH}/function"
+export LAXPROP="${LAXMODULEPATH}/.prop"
 export LAXCORE="ad1e71b1"
+export LAXFUN="source $LAXFUNLOC"
 
 if [ -f "$LAXPROP" ]; then
   dos2unix "$LAXPROP"
   source "$LAXPROP"
 fi
 
-mkdir -p "$LAXBIN"
-mkdir -p "$LAXCASH"
-mkdir -p "$LAXMODULE"
+mkdir -p "$LAXBINPATH"
+mkdir -p "$LAXCASHPATH"
+mkdir -p "$LAXMODULEPATH"
 
 currentCore=$(dumpsys package "$LAXPKG" | grep "signatures" | cut -d '[' -f 2 | cut -d ']' -f 1)
 echo $currentCore
@@ -27,7 +27,7 @@ echo $currentCore
 [ -z "$LAXPKG" ] || [ "$LAXPKG" != "com.appzero.axeron" ] && { echo "Something wrong, may need an update?" && exit 1; }
 echo "$LAXCORE" | grep -q "$currentCore" || { echo "Axeron Not Original" && exit 1; }
 
-functionApi="${LAXMAIN}/fun/function.sh"
+functionApi="${LAXMAINPATH}/fun/function.sh"
 responsePath="${LAXPATH}/response"
 errorPath="${LAXPATH}/error"
 
@@ -38,10 +38,10 @@ am startservice -n "${LAXPKG}/.Storm" --es api "$functionApi" > /dev/null 2>&1
 while [ ! -e "$responsePath" ] && [ ! -e "$errorPath" ]; do
 done
 
-if [ -e "$responsePath" ]; then
-    cp "$responsePath" "$LAXFUNPATH"
-    chmod +x "$LAXFUNPATH"
+[ -e "$responsePath" ] && { cp "$responsePath" "$LAXFUNLOC" && chmod +x "$LAXFUNLOC"; }
+
+if [ -f "$LAXFUNLOC" ]; then
+    $LAXFUN
 else
     echo "LAX Function not found :("
 fi
-[ -f "$LAXFUNPATH" ] && $LAXFUN
