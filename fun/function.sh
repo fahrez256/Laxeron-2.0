27a138bfe950ca899f81c57875e17b9eae447e71
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-08-17 21:18:00 +0700

    Update function.sh

--- fun/function.sh
+++ fun/function.sh
@@ -1,1 +1,122 @@
-echo "Hello Im the Function $LAXPKG :)"
+#global
+whitelist_file="${LAXMODULE}/.config/whitelist.list"
+cachePath="${LAXMODULE}/.cache"
+urlBin="${LAXMAIN}/bin"
+
+#color
+ORANGE='\033[38;2;255;85;3m'
+GREY='\033[38;2;105;105;105m'
+NC='\033[0m'
+
+import() {
+  	filename="$1"
+  	file=$(find "$(dirname "$0")" -type f -name "$filename")
+  	
+  	if [ -z "$file" ]; then
+  		    dir="$(dirname "$0")"
+  		    while [ "$dir" != "$LAXCASH" ]; do
+  			        # Cari file di direktori saat ini
+  			        file=$(find "$dir" -maxdepth 1 -name "$filename")
+  			        if [ -n "$file" ]; then
+  			            file="$file"
+  			            break
+  			        fi
+  			        dir="$(dirname "$dir")"
+  		    done
+  	fi
+  	dos2unix $file
+  	source $file
+   	eval path_$(echo "$filename" | tr -cd '[:alnum:]_-')="$file"
+}
+
+rozaq() {
+    if [ -z "$1" ]; then
+		  echo "Error: No text provided."
+		  return 1
+	  fi
+	
+	  echo "r17$(echo -n "$1" | base64 | tr A-Za-z R-ZA-Qr-za-q)"
+}
+
+storm() {
+    local exec=false
+    local save=false
+    local cacheExec=false
+    local usePath=false
+    local file_name="response"
+    local runPath="$LAXPATH"
+
+    if [ $# -eq 0 ]; then
+        echo "Usage: storm <URL> [options]"
+        return 0
+    fi
+
+    case $1 in
+        --runPath|-rP)
+            if [ -d "$2" ]; then
+                runPath="$2"
+                shift 2
+            else
+                shift 1
+            fi
+            ;;
+    esac
+
+    local responsePath="${LAXPATH}/response"
+    local errorPath="${LAXPATH}/error"
+
+    case $1 in
+        --exec|-x) 
+            exec=true
+            api=$([[ "${2:0:3}" = "r17" ]] && echo "${2:3}" | tr R-ZA-Qr-za-q A-Za-z | base64 -d || echo "$2")
+            shift 2
+            ;;
+        --save|-s)
+            save=true
+            api=$([[ "${2:0:3}" = "r17" ]] && echo "${2:3}" | tr R-ZA-Qr-za-q A-Za-z | base64 -d || echo "$2")
+            shift 2
+            ;;
+        *)
+            api=$1
+            shift
+            ;;
+    esac
+
+    case $1 in
+        --fname|-fn)
+            file_name="$2"
+            rm -f "${runPath}/$file_name"
+            shift 2
+            ;;
+    esac
+
+    if [ -z "$api" ]; then
+        echo "Error: No API URL provided."
+        return 1
+    fi
+
+    rm -f "$responsePath"
+    rm -f "$errorPath"
+
+    am startservice -n "${LAXPKG}/.Storm" --es api "$api" > /dev/null 2>&1
+
+    # Menunggu hingga file response atau error ada dengan delay
+    while [ ! -e "$responsePath" ] && [ ! -e "$errorPath" ]; do
+        sleep 1
+    done
+
+    if [ -e "$responsePath" ]; then
+        if [ "$exec" = true ]; then
+            cp "$responsePath" "${runPath}/$file_name"
+            chmod +x "${runPath}/$file_name"
+            "${runPath}/$file_name" "$@"
+        elif [ "$save" = true ]; then
+            cp "$responsePath" "${runPath}/$file_name"
+            chmod +x "${runPath}/$file_name"
+        else
+            cat "$responsePath"
+        fi
+    elif [ -e "$errorPath" ]; then
+        cat "$errorPath"
+    fi
+}
