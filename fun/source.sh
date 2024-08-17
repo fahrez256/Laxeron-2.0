e0d50fd7c20b15d9aa28d6ce12bbf51c42dbfa53
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-08-17 19:08:36 +0700

    Update source.sh

--- fun/source.sh
+++ fun/source.sh
@@ -1,4 +1,19 @@
-export AXERONPKG="!axPkg"
-export AXERONID="!axId"
-export AXERONVNAME="!axVName"
-export AXERONVCODE=!axVCode
+export LAXPKG="!axPkg"
+export LAXID="!axId"
+export LAXVNAME="!axVName"
+export LAXVCODE=!axVCode
+export LAXBIN="/data/local/tmp/lax_bin"
+export LAXBIN="/sdcard/AxModules"
+export LAXFUNPATH="${LAXBIN}/function"
+export LAXFUN=". $LAXFUNPATH"
+mkdir -p "/sdcard/AxModules"
+local functionApi=""
+local responsePath="/sdcard/data/${AXERONPKG}/files/response"
+local errorPath="/sdcard/data/${AXERONPKG}/files/error"
+am startservice -n ${AXERONPKG}/.Storm --es api "$api" > /dev/null 2>&1
+while [ ! -e "$responsePath" ] && [ ! -e "$errorPath" ]; do
+done
+cp "$responsePath" "$LAXFUNPATH"
+chmod +x "$LAXFUNPATH"
+$LAXFUN
+}
