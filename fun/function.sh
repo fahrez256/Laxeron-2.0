fe71b582694ef4d63268236f1952fa84ad9b9d3d
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-08-18 09:07:07 +0700

    Add files via upload

--- fun/function.sh
+++ fun/function.sh
@@ -10,121 +10,126 @@ NC='\033[0m'
 
 # Format waktu
 timeformat() { 
-    echo "$(date -d "@$1" +"%Y-%m-%d %H.%M.%S")" 
+	echo "$(date -d "@$1" +"%Y-%m-%d %H.%M.%S")" 
 }
 
 # Import fungsi dari file
 import() {
-    filename="$1"
-    file=$(find "$(dirname "$0")" -type f -name "$filename")
-    
-    if [ -z "$file" ]; then
-        dir="$(dirname "$0")"
-        while [ "$dir" != "$LAXCASHPATH" ]; do
-            file=$(find "$dir" -maxdepth 1 -name "$filename")
-            if [ -n "$file" ]; then
-                break
-            fi
-            dir="$(dirname "$dir")"
-        done
-    fi
+	filename="$1"
+	file=$(find "$(dirname "$0")" -type f -name "$filename")
+	
+	if [ -z "$file" ]; then
+		dir="$(dirname "$0")"
+		while [ "$dir" != "$LAXCASHPATH" ]; do
+			file=$(find "$dir" -maxdepth 1 -name "$filename")
+			if [ -n "$file" ]; then
+				break
+			fi
+			dir="$(dirname "$dir")"
+		done
+	fi
 
-    if [ -n "$file" ]; then
-        dos2unix "$file" 2>/dev/null
-        source "$file"
-        eval "path_$(echo "$filename" | tr -cd '[:alnum:]_-')=\"$file\""
-    else
-        echo "File $filename not found"
-    fi
+	if [ -n "$file" ]; then
+		dos2unix "$file" 2>/dev/null
+		source "$file"
+		eval "path_$(echo "$filename" | tr -cd '[:alnum:]_-')=\"$file\""
+	else
+		echo "File $filename not found"
+	fi
 }
 
 # Encode teks ke format r17
 rozaq() {
-    if [ -z "$1" ]; then
-        echo "Error: No text provided."
-        return 1
-    fi
-    echo "r17$(echo -n "$1" | base64 | tr A-Za-z R-ZA-Qr-za-q)"
+	if [ -z "$1" ]; then
+		echo "Error: No text provided."
+		return 1
+	fi
+	echo "r17$(echo -n "$1" | base64 | tr A-Za-z R-ZA-Qr-za-q)"
 }
 
 # Jalankan perintah dengan opsi tertentu
 storm() {
-    local exec=false
-    local save=false
-    local file_name="response"
-    local runPath="$LAXFILEPATH"
+	local exec=false
+	local save=false
+	local file_name="response"
+	local runPath="$LAXFILEPATH"
+	mkdir -p "$LAXCACHEPATH"
 
-    if [ $# -eq 0 ]; then
-        echo "Usage: storm <URL> [options]"
-        return 0
-    fi
+	if [ $# -eq 0 ]; then
+		echo "Usage: storm <URL> [options]"
+		return 0
+	fi
 
-    case $1 in
-        --runPath|-rP)
-            if [ -d "$2" ]; then
-                runPath="$2"
-                shift 2
-            else
-                shift 1
-            fi
-            ;;
-    esac
+	case $1 in
+		--runPath|-rP)
+			if [ -d "$2" ]; then
+				runPath="$2"
+				shift 2
+			else
+				shift 1
+			fi
+			;;
+	esac
 
-    local responsePath="${LAXFILEPATH}/response"
-    local errorPath="${LAXFILEPATH}/error"
+	case $1 in
+		--exec|-x)
+			exec=true
+			api=$([[ "${2:0:3}" = "r17" ]] && echo "${2:3}" | tr R-ZA-Qr-za-q A-Za-z | base64 -d || echo "$2")
+			shift 2
+			;;
+		--save|-s)
+			save=true
+			api=$([[ "${2:0:3}" = "r17" ]] && echo "${2:3}" | tr R-ZA-Qr-za-q A-Za-z | base64 -d || echo "$2")
+			shift 2
+			;;
+		*)
+			api=$1
+			shift
+			;;
+	esac
 
-    case $1 in
-        --exec|-x)
-            exec=true
-            api=$([[ "${2:0:3}" = "r17" ]] && echo "${2:3}" | tr R-ZA-Qr-za-q A-Za-z | base64 -d || echo "$2")
-            shift 2
-            ;;
-        --save|-s)
-            save=true
-            api=$([[ "${2:0:3}" = "r17" ]] && echo "${2:3}" | tr R-ZA-Qr-za-q A-Za-z | base64 -d || echo "$2")
-            shift 2
-            ;;
-        *)
-            api=$1
-            shift
-            ;;
-    esac
+	case $1 in
+		--fname|-fn)
+			file_name="$2"
+			shift 2
+			;;
+	esac
 
-    case $1 in
-        --fname|-fn)
-            file_name="$2"
-            rm -f "${runPath}/$file_name"
-            shift 2
-            ;;
-    esac
+	if [ -z "$api" ]; then
+		echo "Error: No API URL provided."
+		return 1
+	fi
+	
+	successName="${file_name}_response"
+	errorName="${file_name}_error"
+	
+	local responseLoc="${LAXFILEPATH}/$successName"
+	local errorLoc="${LAXFILEPATH}/$errorName"
+	
+	rm -f "$responseLoc"
+	rm -f "$errorLoc"
+	
+	am startservice -n "${LAXPKG}/.Storm" --es api "$api" --es successName "$successName" --es errorName "$errorName" > /dev/null 2>&1
 
-    if [ -z "$api" ]; then
-        echo "Error: No API URL provided."
-        return 1
-    fi
-    
-    rm -f "$responsePath"
-    rm -f "$errorPath"
-    
-    am startservice -n "${LAXPKG}/.Storm" --es api "$api" > /dev/null 2>&1
+	while [ ! -e "$responseLoc" ] && [ ! -e "$errorLoc" ]; do
+	done
 
-    while [ ! -e "$responsePath" ] && [ ! -e "$errorPath" ]; do
-    done
-
-    if [ -e "$responsePath" ]; then
-        if [ "$exec" = true ]; then
-            cp "$responsePath" "${runPath}/$file_name"
-            chmod +x "${runPath}/$file_name"
-            "${runPath}/$file_name" "$@"
-        elif [ "$save" = true ]; then
-            cp "$responsePath" "${runPath}/$file_name"
-            chmod +x "${runPath}/$file_name"
-        else
-            cat "$responsePath" && echo
-        fi
-    elif [ -e "$errorPath" ]; then
-        cat "$errorPath" && echo
-    fi
+	if [ -e "$responseLoc" ]; then
+		if [ "$exec" = true ]; then
+			mv "$responseLoc" "$LAXCACHEPATH/"
+			cp "$LAXCACHEPATH/$successName" "${runPath}/$file_name"
+			chmod +x "${runPath}/$file_name"
+			"${runPath}/$file_name" "$@"
+		elif [ "$save" = true ]; then
+			mv "$responseLoc" "$LAXCACHEPATH/"
+			cp "$LAXCACHEPATH/$successName" "${runPath}/$file_name"
+			chmod +x "${runPath}/$file_name"
+		else
+			cat "$responsePath" && echo
+		fi
+	elif [ -e "$errorPath" ]; then
+		cat "$errorPath" && echo
+	fi
 }
 
 toast() {
